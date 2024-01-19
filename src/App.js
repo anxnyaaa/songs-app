e37f25638b458b5f6af5cb01efb6c0d8d286c228
Ananya Pal <ananya.pal@iemlabs.com>
2024-01-19 16:05:46 +0530

    1st

--- src/App.js
+++ src/App.js
@@ -1,4 +1,38 @@
+import React from 'react'
+import './App.css';
 import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className='container'>
+      <div className='top'>
+        <div className='left'>
+          <div className='sidebar'>
+            <img src={logo} className="App-logo" alt="logo" />
+            <h5>Home</h5>
+            <h5>Search</h5>    
+          </div>
+          <div className='library'>
+            <div className='heading'><h5>Your Library</h5></div>
+            <div className='first-playlist'>First playlist</div>
+            <div className='podcast'>Podcast</div>
+            <div className='footer'></div>
+          </div>
+        </div>
+        <div className='right'>
+          <div className='header'></div>
+          <div className='playlists'></div>
+          <div className='footer'></div>          
+        </div>
+      </div>
+      <div className='down'></div>
+    </div>
+  )
+}
+
+export default App
+
+/*import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -22,4 +56,4 @@ function App() {
   );
 }
 
-export default App;
+export default App;*/
